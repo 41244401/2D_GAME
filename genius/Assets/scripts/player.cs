070e6384a5eb8660d2ba116296c8fc2bb64a98db
41244401 <56706499+41244401@users.noreply.github.com>
2019-11-15 12:11:58 +0800

    按鍵控制

--- genius/Assets/scripts/player.cs
+++ genius/Assets/scripts/player.cs
@@ -2,12 +2,48 @@
 
 public class player : MonoBehaviour
 {
+    #region 欄位區域
     [Header("速度"), Range(0f,100f)]
-    public float speed = 3.5f;
+    public float speed = 10f;
     [Header("跳躍"), Range(100, 2000)]
     public int jump = 300;
     [Header("是否在地上"), Tooltip("用來判定角色是否在地板上。")]
     public bool isGround = false;
     [Header("名字")]
     public string _name = "Dragon King";
+    [Header("元件")]
+    public Rigidbody2D r2d;
+    public Animator ani;
+    #endregion
+
+    private void Move()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        r2d.AddForce(new Vector2(speed * h, 0));
+        ani.SetBool("跑開關", h != 0);
+    }
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGround == true)
+        {
+            isGround = false;
+            r2d.AddForce(new Vector2(0, jump));
+        }
+    }
+    private void Dead()
+    {
+
+    }
+    private void Update()
+    {
+        Move();
+        Jump();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "地板")
+        {
+            isGround = true;
+        }
+    }
 }
